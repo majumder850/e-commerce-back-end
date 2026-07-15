ec90c7ee33e7f836b46edc966351b355ab66e5bb
majumder850 <majumder850@gmail.com>
2026-07-15 06:51:40 +0530

    updated

--- db/db.js
+++ db/db.js
@@ -3,12 +3,27 @@ require("dotenv").config();
 
 const mongoUri = process.env.MONGODB;
 
+
+let isConnected = false;
+
 const initializeDatabase = async () => {
+  
+  if (isConnected) {
+    console.log("=> Using existing database connection");
+    return;
+  }
+
   try {
-    await mongoose.connect(mongoUri);
+    console.log("=> Creating new database connection");
+    const db = await mongoose.connect(mongoUri, {
+      serverSelectionTimeoutMS: 5000, 
+    });
+  
+    isConnected = db.connections[0].readyState;
     console.log("Connected to Database");
   } catch (error) {
-    console.error("Error connecting to Database:", error);
+    console.error("Error connecting to Database:", error.message);
+    throw error; 
   }
 };
 
